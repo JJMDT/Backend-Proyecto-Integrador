5fea23bba00bece280ceb6eacd9acdd951ba422e
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-11-15 14:47:13 -0300

    se agrego verifytoken al crear un nuevo shift

--- src/routes/shiftRoutes.ts
+++ src/routes/shiftRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as shiftController from '../controllers/shiftController';
+import { verifyToken } from '../middlewares/authMiddleware';
 
 const router = Router();
 
@@ -12,8 +13,8 @@ router.get('/:id', shiftController.getShiftById);
 // GET /api/shifts/user/:idUser - Obtener turnos por usuario
 router.get('/user/:idUser', shiftController.getShiftsByUser);
 
-// POST /api/shifts - Crear un nuevo turno
-router.post('/', shiftController.createShift);
+// POST /api/shifts - Crear un nuevo turno - verificar token
+router.post('/',verifyToken, shiftController.createShift);
 
 // mostrar todos los turnos de un profesional en especifico
 export default router;
