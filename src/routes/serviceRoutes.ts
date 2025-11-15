1a518105b74f6565128d88c050018fad9be694e1
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-11-15 18:55:28 -0300

    rutas seguras

--- src/routes/serviceRoutes.ts
+++ src/routes/serviceRoutes.ts
@@ -1,10 +1,10 @@
 import { Router} from "express";
 import { createService, getAllServices } from "../controllers/serviceController";
-
+import { verifyToken } from '../middlewares/authMiddleware'
 const router = Router();
 
 // Ruta para crear un nuevo servicio
-router.post("/", createService);
+router.post("/", verifyToken, createService);
 // Ruta para obtener todos los servicios
 router.get("/", getAllServices);
 
