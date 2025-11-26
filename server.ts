16c3adb279989a7cf95159724153d4ce63b091eb
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-11-25 22:42:08 -0300

    funcionalidad eliminar servicio

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 // Configuración del servidor
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || 'localhost';
 
 //aca ejecuto mis tareas que estan con la libreria Cron
 loadCronJobs()
